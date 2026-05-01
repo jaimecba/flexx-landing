881f4f3a9898b23b3c193c9108926bb4a74edd24
jaimecba <jaimecba@gmail.com>
2026-04-30 22:45:15 -0400

    corrigindo erro de sintaxe metrics

--- app/components/Metrics.tsx
+++ app/components/Metrics.tsx
@@ -83,8 +83,10 @@ const metricVariants = {
     show: {
         opacity: 1,
         y: 0,
-        transition: { duration: 0.6, ease: [0.22, 1, 0.36, 1] },
-    },
+        transition: {
+            duration: 0.6, ease: "easeOut"
+        },
+    }
 };
 
 const iconVariants = {
