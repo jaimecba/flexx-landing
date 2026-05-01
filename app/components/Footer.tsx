45647597f80de7f6e83b311afcdd4356e865b43a
jaimecba <jaimecba@gmail.com>
2026-04-30 21:45:59 -0400

    corrigindo icone github

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@
 
 import { motion } from 'framer-motion';
 import Link from 'next/link';
-import { Github, Twitter, Instagram, Linkedin } from 'lucide-react';
+import { GithubIcon, Twitter, Instagram, Linkedin } from 'lucide-react';
 
 const Footer = () => {
     const fadeUpVariants = {
