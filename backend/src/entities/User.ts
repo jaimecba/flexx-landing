b4d01b24051e89ff259ce7738e11113412faa221
jaimecba <jaimecba@gmail.com>
2026-05-07 20:53:04 -0400

    Adiciona tipos explícitos nas entidades do TypeORM

--- backend/src/entities/User.ts
+++ backend/src/entities/User.ts
@@ -3,17 +3,17 @@ import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
 @Entity()
 export class User {
     @PrimaryGeneratedColumn()
-    id: number;
+    id!: number;
 
     @Column()
-    name: string;
+    name!: string;
 
     @Column({ unique: true })
-    email: string;
+    email!: string;
 
     @Column()
-    password: string;
+    password!: string;
 
     @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
-    createdAt: Date;
+    createdAt!: Date;
 }
