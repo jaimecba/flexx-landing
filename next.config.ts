b7b5ea4887e945ea4350633afccfadca66f8dd27
jaimecba <jaimecba@gmail.com>
2026-05-07 22:56:32 -0400

    Exclui backend do tsconfig.json do Next.js

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.0.52"],
-  turbopack: {},
-  webpack: (config, { isServer }) => {
-    // Excluir backend da compilação
-    config.externals.push({
-      typeorm: "typeorm",
-      pg: "pg",
-      "reflect-metadata": "reflect-metadata",
-    });
-
+  webpack: (config) => {
+    config.externals.push(
+      "typeorm",
+      "pg",
+      "reflect-metadata",
+      "bcryptjs",
+      "jsonwebtoken"
+    );
     return config;
   },
 };
