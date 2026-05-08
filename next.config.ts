ba332ae094d3db85e97bb0483f8f244795b5050f
jaimecba <jaimecba@gmail.com>
2026-05-07 20:56:23 -0400

    Configura Next.js para excluir backend da compilação

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.0.52"],
+  webpack: (config, { isServer }) => {
+    // Excluir backend da compilação
+    config.externals.push({
+      typeorm: "typeorm",
+      pg: "pg",
+      "reflect-metadata": "reflect-metadata",
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
