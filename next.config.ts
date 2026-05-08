16cb3972d57ed1263c1345a41ec3579eec24ea06
jaimecba <jaimecba@gmail.com>
2026-05-07 23:07:40 -0400

    Remove webpack config - use Turbopack defaults

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.0.52"],
-  webpack: (config) => {
-    config.externals.push(
-      "typeorm",
-      "pg",
-      "reflect-metadata",
-      "bcryptjs",
-      "jsonwebtoken"
-    );
-    return config;
-  },
 };
 
 export default nextConfig;
