919671088db65d30a1ee89db59ee494ca962ed7e
jaimecba <jaimecba@gmail.com>
2026-05-11 10:48:19 -0400

    Adicionar botão WhatsApp flutuante - fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         {children}
-        {/* <WhatsAppFloatingButton /> */}
+        <WhatsAppFloatingButton />
       </body>
     </html>
   );
