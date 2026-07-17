a564c402071449335fb61da460c81f74f39b8ffd
jaimecba <jaimecba@gmail.com>
2026-07-17 15:38:35 -0400

    feat: adiciona mensagem de origem da demonstracao no WhatsApp

--- config/links.ts
+++ config/links.ts
@@ -7,7 +7,7 @@ export const EXTERNAL_LINKS = {
      * para o contato posterior com a Flexx.
      */
     pontoWebDemo:
-        "https://app.getdemo.com.br/s/371a1b4b-fb47-4fb8-b035-af68783eb874?phone=5565992367994",
+        "https://app.getdemo.com.br/s/371a1b4b-fb47-4fb8-b035-af68783eb874?phone=5565992367994&text=Ol%C3%A1%21+Vim+pela+demonstra%C3%A7%C3%A3o+interativa+do+Ponto+Web+e+gostaria+de+falar+com+um+especialista+da+Flexx.&utm_source=chatgpt.com",
 } as const;
 
 export type ExternalLinkKey = keyof typeof EXTERNAL_LINKS;
